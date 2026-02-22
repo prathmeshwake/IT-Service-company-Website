4f5ddeccecb4324830c6101d817ee5e9d54b8e04
prathmeshwake <89002800+prathmeshwake@users.noreply.github.com>
2026-02-22 22:02:31 +0530

    Update Contact.java

--- src/main/java/com/prathmeshwake/itservices/website/model/Contact.java
+++ src/main/java/com/prathmeshwake/itservices/website/model/Contact.java
@@ -19,6 +19,45 @@ public class Contact {
     private LocalDateTime createdAt = LocalDateTime.now();
 
     public Contact() {}
-    
+
     // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
